4c09841da04dea11dc28e4b873f56cd27bdb0112
Caroline Hvirfvel <carolinehvirfvel@Carolines-MacBook-Pro.local>
2020-11-26 19:02:16 +0000

    responsive header done

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
-/* Hamburger menu */
+// Hamburger menu
 
-function hamMenu(x) {
-    x.classList.toggle("change");
+let menuToggle = document.querySelector("#menuToggle");
+let menuContainer = document.querySelector("#dropdown-container");
+
+menuToggle.addEventListener("click", toggleHamburger);
+
+function toggleHamburger() {
+    menuToggle.classList.toggle("change");
+    menuContainer.classList.toggle("show");
 }
 
+// Categories dropdown
+let categoriesBtn = document.querySelector("#dropdown-categories .dropbtn");
+let categoriesMenu = document.querySelector("#categories-ul");
+
+categoriesBtn.addEventListener("click", toggleCategories);
+
+function toggleCategories() {
+    categoriesMenu.classList.toggle("show");
+}
